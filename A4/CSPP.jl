bcfb26b8645d9f8d7159b4243e3f52ea7646da0b
jaewoo-space <jw.kim@kaist.ac.kr>
2024-03-29 22:43:28 +0900

    test

--- A4/CSPP.jl
+++ A4/CSPP.jl
@@ -20,61 +20,55 @@ using JuMP, HiGHS
 export IPSolver
 
 function IPSolver(data, Tmax, origin, destination)
+    start_node = Int.(data[:, 1])
+    end_node = Int.(data[:, 2])
+    arc_cost = data[:, 3]
+    arc_time = data[:, 4]
+    n_nodes = maximum(vcat(start_node, end_node))
+    n_arcs = length(start_node)
 
-start_node = Int.(data[:, 1])
-end_node = Int.(data[:, 2])
-arc_cost = data[:, 3]
-arc_time = data[:, 4]
-n_nodes = maximum(vcat(start_node, end_node))
-n_arcs = length(start_node)
+    model = Model(HiGHS.Optimizer)
 
-model = Model(HiGHS.Optimizer)
+    @variable(model, x[i = 1:n_arcs], Bin)
 
-@variable(model, x[i = 1:n_arcs], Bin)
+    @objective(model, Min, sum(arc_cost .* x))
 
-@objective(model, Min, sum(arc_cost .* x))
+    @constraint(model, flow_origin, sum(x[start_node .== origin]) - sum(x[end_node .== origin]) == 1)
+    @constraint(model, flow_balance[i = 1:n_nodes; (i != origin) && (i != destination)], sum(x[start_node .== i]) - sum(x[end_node .== i]) == 0)
+    @constraint(model, flow_destination, sum(x[start_node .== destination]) - sum(x[end_node .== destination]) == -1)
 
-@constraint(model, flow_origin, sum(x[start_node .== origin]) - sum(x[end_node .== origin]) == 1)
-@constraint(model, flow_balance[i = 1:n_nodes; (i != origin) && (i != destination)], sum(x[start_node .== i]) - sum(x[end_node .== i]) == 0)
-@constraint(model, flow_destination, sum(x[start_node .== destination]) - sum(x[end_node .== destination]) == -1)
+    @constraint(model, time, sum(arc_time .* x) <= Tmax)
 
-@constraint(model, time, sum(arc_time .* x) <= Tmax)
+    optimize!(model)
 
-optimize!(model)
-
-if primal_status(model) == FEASIBLE_POINT
-    return objective_value(model), value.(x)
-else
-    println("Infeasible instance!")
-    return nothing
-end
-
+    if primal_status(model) == FEASIBLE_POINT
+        return objective_value(model), value.(x)
+    else
+        println("Infeasible instance!")
+        return nothing
+    end
 end
 
 
 function MyCGSolver(data, Tmax, origin, destination)
+    start_node = Int.(data[:, 1])
+    end_node = Int.(data[:, 2])
+    arc_cost = data[:, 3]
+    arc_time = data[:, 4]
+    n_nodes = maximum(vcat(start_node, end_node))
+    n_arcs = length(start_node)
 
-start_node = Int.(data[:, 1])
-end_node = Int.(data[:, 2])
-arc_cost = data[:, 3]
-arc_time = data[:, 4]
-n_nodes = maximum(vcat(start_node, end_node))
-n_arcs = length(start_node)
+    paths = []
+    costs = []
+    times = []
 
-paths = []
-costs = []
-times = []
+    m = Model(HiGHS.Optimizer)
 
-m = Model(HiGHS.Optimizer)
-
-@variable(m, λ[paths] >= 0)
-@objective(m, Min, sum(costs .* λ))
-@constraint(m, resource, sum(times .* λ) <= Tmax)
-@constraint(m, convexity, sum(λ) == 1)
-optimize!(m)
-
-
-
+    @variable(m, λ[paths] >= 0)
+    @objective(m, Min, sum(costs .* λ))
+    @constraint(m, resource, sum(times .* λ) <= Tmax)
+    @constraint(m, convexity, sum(λ) == 1)
+    optimize!(m)
 end
 
 end
