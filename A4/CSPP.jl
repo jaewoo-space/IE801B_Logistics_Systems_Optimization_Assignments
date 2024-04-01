1061a542a0c1755a97286c0529907b2c1f5b6c66
Jaewoo Kim <jw.kim@kaist.ac.kr>
2024-04-01 22:26:18 +0900

    A4 under development

--- A4/CSPP.jl
+++ A4/CSPP.jl
@@ -16,8 +16,11 @@ end
 module Solver
 
 using JuMP, HiGHS
+using Graphs, SimpleWeightedGraphs
 
-export IPSolver
+export IPSolver, cal_cost_time, solve_rmp_and_pricing_problems
+
+const EPS = 1e-10
 
 function IPSolver(data, Tmax, origin, destination)
     start_node = Int.(data[:, 1])
@@ -49,7 +52,63 @@ function IPSolver(data, Tmax, origin, destination)
     end
 end
 
+function cal_cost_time(data, n_arcs, start_node, end_node, path)
+    path_arcs = [findfirst(x->(start_node[x]==path[i]) && end_node[x]==path[i+1], 1:1:n_arcs) for i in 1:1:(length(path)-1)]
+    tpv = sum(data[path_arcs, 3:4], dims=1)
 
+    return path_arcs, tpv[1], tpv[2] # cost and time    
+end
+
+function solve_rmp_and_pricing_problems(path_ind, costs, times, paths, data, Tmax, start_node, end_node, arc_cost, arc_time, origin, destination, n_nodes, n_arcs)
+    M = 100.0
+    
+    while true
+        model = Model(HiGHS.Optimizer)
+
+        @variable(model, y0 >= 0)                                   # artificial variable facilitating the solution procedure
+        @variable(model, λ[path_ind] >= 0)                          # path variables
+        @objective(model, Min, M*y0 + sum(costs .* λ))            # cost minimization
+        @constraint(model, resource, sum(times .* λ) <= Tmax)       # resource constriaint, π1
+        @constraint(model, convexity, y0 + sum(λ) == 1)             # convexity constraint, π0
+        optimize!(model)                                            # solve the relaxaed RMP
+
+        # results
+        y0_val = value(y0)                                      
+        λ_val = value.(λ)
+        obj_val = objective_value(model)
+        π0 = dual(convexity)
+        π1 = dual(resource)
+
+        if y0_val > EPS
+            π0 = M
+        end
+        while true
+            g = SimpleWeightedDiGraph(n_nodes)
+            for i in 1:1:n_arcs
+                add_edge!(g, start_node[i], end_node[i], arc_cost[i] - arc_time[i] * π1)
+            end
+            
+            bf_state = bellman_ford_shortest_paths(g, origin)
+            min_reduced_cost = bf_state.dists[destination] - π0
+            println("min_reduced_cost:$(min_reduced_cost)")
+            if min_reduced_cost < -EPS          # find a new column might improve the solution
+                new_column = enumerate_paths(bf_state)[destination]
+                path_arcs, cost, time = cal_cost_time(data, n_arcs, start_node, end_node, new_column)
+                push!(path_ind, length(path_ind)+1)
+                push!(costs, cost)
+                push!(times, time)
+                push!(paths, path_arcs)
+                break
+            elseif y0_val > EPS                # cannot find a new column, but the solution is infeasible
+                π0 *= 10.0
+            else
+                return path_ind, costs, times, paths, λ_val, obj_val
+            end
+        end
+    end
+end
+
+# TODO
 function MyCGSolver(data, Tmax, origin, destination)
     start_node = Int.(data[:, 1])
     end_node = Int.(data[:, 2])
@@ -58,17 +117,28 @@ function MyCGSolver(data, Tmax, origin, destination)
     n_nodes = maximum(vcat(start_node, end_node))
     n_arcs = length(start_node)
 
-    paths = []
-    costs = []
-    times = []
+    ## initialize
+    path_ind = Int[]
+    costs = Number[]
+    times = Number[]
+    paths = Vector{Int}[]
 
-    m = Model(HiGHS.Optimizer)
+    # get an initial feasible solution
+    g = SimpleWeightedDiGraph(n_nodes)
+    for i in 1:1:n_arcs
+        add_edge!(g, start_node[i], end_node[i], arc_cost[i])
+    end
+    bf_state = bellman_ford_shortest_paths(g, origin)
+    new_column = enumerate_paths(bf_state)[destination]
+    path_arcs, cost, time = cal_cost_time(data, n_arcs, start_node, end_node, new_column)
+    push!(path_ind, length(path_ind)+1)
+    push!(costs, cost)
+    push!(times, time)
+    push!(paths, path_arcs)
 
-    @variable(m, λ[paths] >= 0)
-    @objective(m, Min, sum(costs .* λ))
-    @constraint(m, resource, sum(times .* λ) <= Tmax)
-    @constraint(m, convexity, sum(λ) == 1)
-    optimize!(m)
+    path_ind, costs, times, paths, λ_val, obj_val = solve_rmp_and_pricing_problems(path_ind, costs, times, paths, data, Tmax, start_node, end_node, arc_cost, arc_time, origin, destination, n_nodes, n_arcs)
+
+    return nothing
 end
 
 end
--- A4/test.jl
+++ A4/test.jl
@@ -1,15 +1,44 @@
-using JuMP, HiGHS
+using HiGHS, JuMP, DelimitedFiles
+include("./CSPP.jl")
+using .Tools: CSPP_INSTANCE
+using .Solver: IPSolver, solve_rmp_and_pricing_problems, cal_cost_time
+using JLD
+using Graphs, SimpleWeightedGraphs
 
-function f()
-    model = Model(HiGHS.Optimizer)
-    @variable(model, y >= 0)
-    @constraint(model, y <= 1)
-    @objective(model, Max, y)
-    optimize!(model)
-    return model
+prob = load("prob1.jld")["prob"]
+
+const data = prob.data
+const Tmax = prob.Tmax
+
+const start_node = Int.(data[:, 1])
+const end_node = Int.(data[:, 2])
+const arc_cost = data[:, 3]
+const arc_time = data[:, 4]
+const origin, destination = prob.origin, prob.destination
+const n_nodes = maximum(vcat(start_node, end_node))
+const n_arcs = length(start_node)
+
+## initialize
+path_ind = Int[]
+costs = Number[]
+times = Number[]
+paths = Vector{Int}[]
+
+# get an initial feasible solution
+g = SimpleWeightedDiGraph(n_nodes)
+for i in 1:1:n_arcs
+    add_edge!(g, start_node[i], end_node[i], arc_cost[i])
 end
+bf_state = bellman_ford_shortest_paths(g, origin)
+new_column = enumerate_paths(bf_state)[destination]
+path_arcs, cost, time = cal_cost_time(data, n_arcs, start_node, end_node, new_column)
+push!(path_ind, length(path_ind)+1)
+push!(costs, cost)
+push!(times, time)
+push!(paths, path_arcs)
 
+path_ind, costs, times, paths, λ_val, obj_val = solve_rmp_and_pricing_problems(path_ind, costs, times, paths, data, Tmax, start_node, end_node, arc_cost, arc_time, origin, destination, n_nodes, n_arcs)
 
-tpv = f()
-
-value(all_variables(tpv)[1])
+# TODO
+# if λ is fractional, branch
+# if not, end of the branch
