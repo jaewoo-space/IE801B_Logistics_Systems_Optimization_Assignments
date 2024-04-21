1255ccfc02bf319b98971b44b9ea10bdb2a3c8b9
Jaewoo Kim <tobedynamic@kaist.ac.kr>
2024-04-22 00:11:53 +0900

    Finalize A5

--- A5/ESPPRC.jl
+++ A5/ESPPRC.jl
@@ -6,60 +6,121 @@ export Feillet
 
 const EPS = 1e-6
 
-function dominated_labels(labels, label_length)
+#* name: is_dominated
+# description: check whether label_exm is dominated by label_cmp
+# inputs:
+#   - label_exm: label to examin
+#   - label_cmp: label to compare
+#   - label_length: the length of a label
+# outputs:
+#   - whether label_exm is dominated by label_cmp or not
+function is_dominated(label_exm, label_cmp, label_length)
 
-labels_dom::Array{Tuple} = []
+if sum((label_cmp .- label_exm) .< EPS) == label_length
+    return true
+else
+    return false
+end
 
-for label_check in labels
-    for label_comp in labels
-        if (sum((label_comp .- label_check) .<= EPS) == label_length) && (sum((label_comp .- label_check) .< EPS) > 0)
-            push!(labels_dom, label_check)
+end
+
+#* name: nondominated_labels
+# description: find dominated labels among labels
+# inputs:
+#   - labels: the list of the labels to check
+#   - label_length: the length of a label
+# outputs:
+#   - nondominated labels
+function nondominated_labels(labels, label_length)
+
+n_labels = length(labels)
+
+labels_nd::Array{Bool} = fill(true, n_labels)
+
+for lInd1 in 1:1:(n_labels-1)
+    if labels_nd[lInd1]
+        for lInd2 in (lInd1+1):1:n_labels
+            if labels_nd[lInd2]
+                if is_dominated(labels[lInd1], labels[lInd2], label_length)
+                    labels_nd[lInd1] = false
+                    break
+                end
+                if is_dominated(labels[lInd2], labels[lInd1], label_length)
+                    labels_nd[lInd2] = false
+                end
+            end
         end
     end
 end
 
-return labels_dom
+return labels[labels_nd]
 
 end
 
+#* name: Feillet
+# description: algorithm for solving ESPPRC, proposed by Feillet et al. (2004)
+# inputs:
+#   - capacity: vehicle capacity
+#   - demand: demand at each node
+#   - depot: depot node index
+#   - customers: list of customer indices
+#   - weights: arc weight matrix
+# outputs:
+#   - non-dominated labels list at the destination
+#   - the number of node examinations
 function Feillet(capacity, demand, depot, customers, weights)
 
-nodes = vcat(depot, customers)
-N = length(nodes)
+N = length(customers)
 
-# initialization
-labels_list::Array{Array{Tuple}} = [[] for _ in 1:1:N]
+## 1) initialization
+# define containers and supporting variables
+labels_list::Array{Array{Tuple}} = [[] for _ in 1:1:(N+1)]
 push!(labels_list[depot], (0.0, 0, zeros(Bool, N)..., 0.0))
 E = [depot]
-
-# supporting variables
-F::Array{Tuple} = []
+F_ij::Array{Tuple} = []
 eye = I(N)
 e = [eye[:, i] for i in 1:1:N]
 
+## 2) algorithm
+exm_cnt = 1
 while !isempty(E)
-    i = pop!(E)
+    # a. choose a node to examine
+    i = popfirst!(E)
+    println("============ # Examinations: $exm_cnt, node = $i ==========")
+    # b. extend labels of each successor node
     for j in customers
-        if j != i
-            F = []
+        # if i -> j is an available arc
+        if (j != i) && (weights[i, j] < Inf)
+            F_ij = []
             for label in labels_list[i]
-                if !label[2+j]
-                    new_label = (label[1]+demand[j], label[2]+1, (label[3:(end-1)].+e[j])..., label[end]+weights[i, j])
-                    push!(F, new_label)
+                if !label[1+j]
+                    new_label = (label[1]+demand[j], label[2]+1, Bool.(label[3:(end-1)].+e[j-1])..., round(label[end]+weights[i, j], digits=6))
+                    # capacity constraint
+                    if new_label[1] <= capacity
+                        push!(F_ij, new_label)
+                    end
                 end
             end
-            # EFF
-            labels_dom = dominated_labels(vcat(labels_list[j], F), N+3)
-            new_labels = setdiff(Set(F), Set(labels_dom))
-            if !isempty(new_labels)
-                labels_list[j] = vcat(labels_list[j], collect(new_labels))
-                push!(E, j)
+            # find non-dominated labels
+            labels_nd = nondominated_labels(vcat(labels_list[j], F_ij), N+3)
+            
+            # if the non-dominated labels set of a node is updated, add the node to the examination waiting list
+            if Set(labels_list[j]) != Set(labels_nd)
+                labels_list[j] = labels_nd
+                if !(j in E) && (j != customers[end])
+                    push!(E, j)
+                end
             end
         end
     end
+
+    println("Nodes to examin: $E")
+    tpv = length(labels_list[customers[end]])
+    println("# labels at the destination: $tpv")
+    exm_cnt += 1
 end
 
-return lables_list
+return labels_list[customers[end]], exm_cnt
 
 end
 
--- A5/main.jl
+++ A5/main.jl
@@ -2,16 +2,41 @@ include("ESPPRC.jl")
 using .Solvers: Feillet
 using CVRPLIB, DelimitedFiles
 
+mkpath("./results")
+
 prob_name_list = ["P-n16-k8", "A-n32-k5", "B-n64-k9"]
-pInd = 1
 
-cvrp, _, _ = readCVRPLIB(prob_name_list[pInd])
-dual = readdlm("./data/dual_var_"*prob_name_list[pInd]*".csv")
+exm_cnt = zeros(Int64, 3)
+for pInd in eachindex(prob_name_list)
+    println("****************************************************************")
+    println("**************************  $(prob_name_list[pInd])  **************************")
+    println("****************************************************************")
 
-capacity = cvrp.capacity
-weights = cvrp.weights
-demand = cvrp.demand
-depot = cvrp.depot
-customers = cvrp.customers
+    ## 1) load the instance and duals
+    cvrp, _, _ = readCVRPLIB(prob_name_list[pInd])
+    dual = readdlm("./data/dual_var_"*prob_name_list[pInd]*".csv")
+    
+    ## 2) preprocessing
+    # capacity, demand, depot, and customers
+    capacity = cvrp.capacity
+    demand = vcat(cvrp.demand, cvrp.demand[1])
+    depot = cvrp.depot
+    customers = cvrp.customers
 
-Feillet(capacity, demand, depot, customers, weights .- dual[1:(end-1)])
+    # weights
+    N = length(demand)
+    weights = zeros(Float64, N-1, N)
+    # a. add the destination column
+    weights[:, 1:(N-1)] = cvrp.weights
+    weights[:, N] = weights[:, 1]
+    # b. screening out the depot -> depot arc
+    weights[1, N] = Inf
+
+    ## 3) solve ESPPRC
+    result, exm_cnt[pInd] = Feillet(capacity, demand, depot, customers, weights .- dual[1:end-1])
+
+    ## 4) save the result
+    writedlm("./results/result_$(prob_name_list[pInd]).csv", result, '\t')
+end
+
+writedlm("./results/examinations_count.csv", exm_cnt, '\t')
